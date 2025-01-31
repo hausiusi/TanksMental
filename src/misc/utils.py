bd084c010359fb590c0aa85beb43ccd4f425a8f9
Zviad Mgaloblishvili <hausiusi@gmail.com>
2025-02-01 00:21:57 +0100

    Add angle and raycasting methods to use them later

--- src/misc/utils.py
+++ src/misc/utils.py
@@ -1,7 +1,9 @@
 import os
 import json
+import math
 from pathlib import Path
-from ursina import Texture, Color
+from ursina import Texture, Color, Vec3, raycast, Entity
+
     
 def _serialize_data(data):
     """Convert non-serializable objects to JSON-compatible types."""
@@ -95,3 +97,78 @@ def get_files_in_folder(folder_path: str):
             file_list.append((file_name, full_path))
     
     return file_list
+
+def normalize(v):
+    """Returns the normalized Vec3."""
+    return v.normalized()
+
+def rotate_vector(vec, axis, angle):
+    """
+    Rotates a vector around a given axis by a specified angle using Rodrigues' rotation formula.
+    
+    Parameters:
+    - vec: Vec3, the vector to rotate.
+    - axis: Vec3, the axis to rotate around.
+    - angle: float, rotation angle in radians.
+
+    Returns:
+    - Vec3, the rotated vector.
+    """
+    axis = normalize(axis)
+    cos_theta = math.cos(angle)
+    sin_theta = math.sin(angle)
+    
+    return (vec * cos_theta +
+            axis.cross(vec) * sin_theta +
+            axis * axis.dot(vec) * (1 - cos_theta))
+
+def rotate_vector_around_y(vec, angle):
+    """
+    Rotates a vector around the Z-axis by a specified angle.
+    
+    Parameters:
+    - vec: Vec3, the vector to rotate.
+    - angle: float, rotation angle in radians.
+
+    Returns:
+    - Vec3, the rotated vector.
+    """
+    cos_theta = math.cos(angle)
+    sin_theta = math.sin(angle)
+
+    return Vec3(
+        vec.x * cos_theta - vec.y * sin_theta,  # X rotation
+        vec.x * sin_theta + vec.y * cos_theta,   # Y rotation
+        vec.z  # Y stays the same
+    )
+
+def raycast_around(vec, theta, n_rays, entity:Entity, distance=2, ignore=[], debug=False):
+    """
+    This function is for debugging. It's heavy and affects the framerate dramatically.
+    The better variant is to use frame-by-frame scanning around, that means one raycast per frame
+    with different angles.
+
+    Parameters:
+    - vec: Vec3, the direction vector of the entity
+    - theta: angle that will be raycasted from left to right. e.g. if it's 0.2rad means that 0.2 radians
+        will be raycasted on the left and 0.2 radians on the right
+    - entity: the entity that emits the raycasts
+    - distance: length of raycast
+    - ignore[]: the entities that must be ignored while collecting the ray-hits
+    - debug: if true the rays will be visible and the hit entities will be printed in console
+    """
+    theta_step = theta / n_rays
+    theta_current = 0
+    for i in range(n_rays):
+        theta_current += theta_step        
+        slightly_left = rotate_vector_around_y(vec, theta_current)
+        slightly_right = rotate_vector_around_y(vec, -theta_current)
+        ray_left = raycast(entity.position, slightly_left, ignore=ignore, distance=distance, debug=debug)
+        ray_right = raycast(entity.position, slightly_right, ignore=ignore, distance=distance, debug=debug)
+
+        # TODO: collect all entities and return them
+        if debug:
+            if ray_left.hit:
+                print(ray_left.entities)
+            if ray_right.hit:
+                print(ray_right.entities)
